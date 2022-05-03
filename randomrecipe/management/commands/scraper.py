2f792e61fb2c2b357807144ffc7415a08168b592
Bailey <bc2412@gmail.com>
2022-05-03 09:39:28 -0400

    pull name, ingredients and instructions correctly

--- randomrecipe/management/commands/scraper.py
+++ randomrecipe/management/commands/scraper.py
@@ -18,8 +18,8 @@ class Command(BaseCommand):
 
             return driver
 
-        def get_recipe(name, link):
-            ingredients_list, result_ingr, result_instruct = [], '', ''
+        def get_recipe(link):
+            ingredients_list, instructions_list, result_ingr, result_instruct, recipe_name = [], [], '', '', ''
 
             # create a driver for concrete page
             recipe_driver = connect(link)
@@ -27,21 +27,64 @@ class Command(BaseCommand):
             retry = 1
             while retry <= 10:
                 try:
-                    result_ingr = recipe_driver.find_element(by=By.XPATH, value=f"//*[@id='content']/div[1]/div/div[{retry}]/div[1]/div[1]").text
-                    result_instruct = recipe_driver.find_element(by=By.XPATH, value=f"//*[@id='content']/div[1]/div/div[{retry}]/div[1]/div[2]/ol").text
-                    recipe_driver.close()
-                    break
-                except Exception:
+                    # Delish
+                    if 'delish' in link:
+                        recipe_name = link.split("/")[-2].replace("-", " ").replace("recipe", "").title()
+                        result_ingr = recipe_driver.find_element(by=By.XPATH, value="/html/body/main/div[5]/div[1]/div[6]/div[1]/div[2]/div[1]/div[2]").text
+                        result_instruct = recipe_driver.find_element(by=By.XPATH, value="/html/body/main/div[5]/div[1]/div[6]/div[2]/div[2]/div/div[2]/ol").text
+                        recipe_driver.close()
+                        break
+
+                    # Tasty
+                    elif 'tasty' in link:  # TODO tasty breaks up their ingredients per item in the recipe, can turn into dict for better viewing
+                        recipe_name = link.split("/")[-1].replace("-", " ").title()
+                        result_ingr = recipe_driver.find_element(by=By.XPATH, value="//*[@id='content']/div[1]/div/div[4]/div[1]/div[1]").text
+                        result_instruct = recipe_driver.find_element(by=By.XPATH, value="//*[@id='content']/div[1]/div/div[4]/div[1]/div[2]/ol").text
+                        recipe_driver.close()
+                        break
+
+                    # Simply Recipes
+                    if 'simply' in link:
+                        recipe_name = link.split("/")[-1].replace("-", " ")
+                        recipe_name = ''.join([i for i in recipe_name if not i.isdigit()]).title()
+                        result_ingr = recipe_driver.find_element(by=By.XPATH, value="//*[@id='structured-ingredients_1-0']/ul").text
+                        result_instruct = recipe_driver.find_element(by=By.XPATH, value="//*[@id='mntl-sc-block_3-0']").text
+                        recipe_driver.close()
+                        break
+
+                    # All Recipes
+                    if 'all' in link:
+                        recipe_name = link.split("/")[-2].replace("-", " ").title()
+                        result_ingr = recipe_driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/main/div[1]/div[2]/div[1]/div[2]/div[2]/div[5]/section[1]/fieldset/ul").text
+                        result_instruct = recipe_driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/main/div[1]/div[2]/div[1]/div[2]/div[2]/section[1]/fieldset/ul").text
+                        recipe_driver.close()
+                        break
+                except Exception as e:
+                    print(e)
                     retry += 1
 
             ingredients = result_ingr.split("\n")
-            instructions_list = result_instruct.split("\n")
+            instructions = result_instruct.split("\n")
 
             for data in ingredients:
                 if not bad_ingredients(data):
+                    data = data.replace(",", "").replace(", ", "")
                     ingredients_list.append(data)
 
-            recipe = Recipe(name=name, link=link, ingredients=ingredients_list, instructions=instructions_list)
+            for instruction in instructions:
+                if 'simply' in link and ':' in instruction:
+                    continue
+
+                if 'all' in link and 'step ' in instruction.lower():
+                    continue
+
+                instruction = instruction.replace(",", "").replace(", ", "")
+                instructions_list.append(instruction)
+
+            print(instructions_list)
+            print(ingredients_list)
+
+            recipe = Recipe(name=recipe_name, link=link, ingredients=ingredients_list, instructions=instructions_list)
             recipe.save()
 
         def bad_ingredients(data):
@@ -58,4 +101,4 @@ class Command(BaseCommand):
 
             return ignore
 
-        get_recipe(name='Fajita Bowls', link='https://tasty.co/recipe/portobello-fajita-bowl-meal-prep')
+        get_recipe(link='https://www.allrecipes.com/recipe/8376893/chicken-chilaquiles-verdes/')
